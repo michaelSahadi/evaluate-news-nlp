01645caf7071a441c83b147de6b130c1ac3ff2f7
michaelSahadi <michael@michaelsahadi.com>
2021-12-01 03:14:41 +0000

    webpack dev stuff

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,11 +2,15 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    devServer: {
+        port: 3000
+    },
     module: {
         rules: [
             {
